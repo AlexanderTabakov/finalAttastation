a351f5e7e846afece2ba25d71fb4c8c126a1eef6
AlexanderTabakov <sir.alexander.tabakov@gmail.com>
2023-04-17 14:58:48 +0700

    Test Deploy

--- my-app/src/components/ProductCard/ProductCard.jsx
+++ my-app/src/components/ProductCard/ProductCard.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 
+import {products} from "../../pages/ProducsPage/Products";
+
 import './ProductCard.css'
 
-import '../../assets/Oysters.svg'
-import '../../assets/Ribs.svg'
-import '../../assets/Shrimps.svg'
+// import '../../assets/Oysters.svg'
+// import '../../assets/Ribs.svg'
+// import '../../assets/Shrimps.svg'
 
 // const ProductCard = (image, title, description) => {
-const ProductCard = () => {
+const ProductCard = ({image, title, description, price}) => {
     return (
         <div className={'card'}>
-            <img className={'image'} src='../../assets/Shrimps.svg' alt="product"/>
-            <p className={'text title'}>NNNNNNNNN</p>
-            <p className={'text description'}>DDDDDDDDDDDDDDDDD</p>
+            <img className={'image'} src={image} alt="product"/>
+            <p className={'text title'}>{title}</p>
+            <p className={'text description'}>{description}</p>
+            <div className={'wrapper'}>
+                <p className={'text price'}>{price} ₽</p>
+                <button className={'addToCartBtn'}>+</button>
+            </div>
+
+
 
         </div>
     )
--- my-app/src/pages/ProductPage.jsx
+++ my-app/src/pages/ProductPage.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import './ProductsPage.css'
 import ProductCard from "../components/ProductCard/ProductCard";
+import {products} from "./ProducsPage/Products";
 
 const ProductPage = () => {
 
     return (
-        <div className={'product-wrapper'}>
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+
+
+
+        <div className={'product-wrapper'}> {
+            products.map(product =>
+                <ProductCard
+                key={product.id}
+                image={product.image}
+                title={product.title}
+                description={product.description}
+                price={product.price}
+                />
+            )
+        };
+
         </div>
     )
 
